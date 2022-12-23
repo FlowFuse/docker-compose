3fc832beab11177b30c7e8f584d923e6ad9bb801
François Lecoufle <flecoufle@gmail.com>
2022-12-23 12:28:39 +0100

    Fix identifiers with double quotes needed for the db name

--- setup-context-db.sh
+++ setup-context-db.sh
@@ -3,6 +3,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 -U root <<-ESQL
-  CREATE DATABASE ff-context;
-  GRANT ALL PRIVILEGES ON DATABASE ff-context TO forge;
+  CREATE DATABASE "ff-context";
+  GRANT ALL PRIVILEGES ON DATABASE "ff-context" TO "forge";
 ESQL
