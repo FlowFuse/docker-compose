8a1d8555ea2e3d9387835b497438905286684529
Ben Hardill <ben@flowforge.com>
2023-01-11 14:48:40 +0000

    Only create DB if it doesn't exist

--- setup-context-db.sh
+++ setup-context-db.sh
@@ -3,6 +3,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 -U root <<-ESQL
-  CREATE DATABASE "ff-context";
+  SELECT 'CREATE DATABASE "ff-context"' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'ff-context')\gexec
   GRANT ALL PRIVILEGES ON DATABASE "ff-context" TO "forge";
 ESQL
