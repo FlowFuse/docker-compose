9cc1a5c5e9ffbc124a5efe7d3836a0b8fe863ae3
Ben Hardill <ben@flowforge.com>
2024-12-18 13:37:02 +0000

    Add node-red editor cache files (#181)

---
+++ flowforge-docker/install-device-cache.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -x
+
+VERSION_LIST="4.0.0 4.0.1 4.0.2 4.0.3 4.0.4 4.0.5 4.0.7"
+
+mkdir -p device/cache
+cd device/cache
+
+pwd
+
+for V in $VERSION_LIST
+do  
+  echo $V
+  mkdir $V
+  ls -l
+  npm install --omit=dev --omit=optional --no-audit --no-fund --prefix "$V" "@node-red/editor-client@$V"
+done
+
+pwd
+ls -l
+du -sh
