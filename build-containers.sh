3f877b73e53a6a4eb9544fa28563008923762f49
Ben Hardill <ben@flowforge.com>
2025-07-04 11:53:36 +0100

    Rename flowforge-docker container
    linked to FlowFuse/admin#405

--- build-containers.sh
+++ build-containers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build flowforge-docker -t flowfuse/forge-docker
+docker build flowfuse-docker -t flowfuse/forge-docker
 docker build node-red-container -t flowfuse/node-red
 docker build file-server -t flowfuse/file-server
